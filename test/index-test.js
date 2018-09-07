d38dda881d178fc51983335bf40eaa7ed759c97a
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-07 14:28:56 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -75,6 +75,7 @@ describe('Handlebars Templates Lab', function() {
     describe('createRecipe', function() {
       it('renders the recipe template', function() {
         init()
+        debugger;
         var spy = expect.spyOn(window.Handlebars, "compile").andCallThrough()
         createRecipe()
         expect(spy).toHaveBeenCalledWith(document.getElementById("recipe-template").innerHTML)
